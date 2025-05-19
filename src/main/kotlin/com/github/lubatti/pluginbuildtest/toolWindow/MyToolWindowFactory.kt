ff1b58e2424651bd1c46de9972c83bdc2b597566
GitHub Action <action@github.com>
2025-05-19 09:33:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/lubatti/pluginbuildtest/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lubatti/pluginbuildtest/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.lubatti.pluginbuildtest.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lubatti.pluginbuildtest.MyBundle
+import com.github.lubatti.pluginbuildtest.services.MyProjectService
 import javax.swing.JButton
 
 
